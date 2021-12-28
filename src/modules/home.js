8b1a5c53186d50672d06738685d4936a6824ccac
eryner-z <stfn.bgr@gmail.com>
2021-12-28 18:02:16 +0100

    change head section

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,15 +1,127 @@
-
-
 const createBackground = () => {
     const image = document.createElement("img");
     image.classList.add("background-image")
     image.src = "../src/images/ramen-background.jpg";
     image.alt = "tasty ramen";
-    console.log(image);
     return image
 }
 
+const createWrapperUpper = () => {
+    const wrapperU = document.createElement("div");
+    wrapperU.classList.add("wrapper-upper");
+    return wrapperU
+}
+
+const createNavWrap = () => {
+    const navWrap = document.createElement("div");
+    navWrap.classList.add("nav-wrapper");
+    return navWrap
+}
+
+const createNav = () => {
+
+    const nav = document.createElement("nav");
+    nav.classList.add("nav");
+
+    const navUl = document.createElement("ul");
+    nav.appendChild(navUl);
+
+    const li1 = document.createElement("li");
+    navUl.appendChild(li1);
+    const li1A = document.createElement("a");
+    li1.appendChild(li1A);
+    li1A.href = "#";
+    li1A.innerText = "Menu";
+
+    const li2 = document.createElement("li");
+    navUl.appendChild(li2);
+    const li2A = document.createElement("a");
+    li2.appendChild(li2A);
+    li2A.href = "#";
+    li2A.innerText = "Reservations";
+
+    const li3 = document.createElement("li");
+    navUl.appendChild(li3);
+    const li3A = document.createElement("a");
+    li3.appendChild(li3A);
+    li3A.href = "#";
+    li3A.innerText = "Contact";
+
+    const so1 = document.createElement("li");
+    so1.classList.add("social");
+    navUl.appendChild(so1);
+    const so1A = document.createElement("a");
+    so1.appendChild(so1A);
+    so1A.href = "#";
+    const icon1 = document.createElement("i");
+    icon1.classList.add("fa", "fa-facebook");
+    so1A.appendChild(icon1);
+
+    const so2 = document.createElement("li");
+    so2.classList.add("social");
+    navUl.appendChild(so2);
+    const so2A = document.createElement("a");
+    so2.appendChild(so2A);
+    so2A.href = "#";
+    const icon2 = document.createElement("i");
+    icon2.classList.add("fa", "fa-twitter");
+    so2A.appendChild(icon2);
+
+    const so3 = document.createElement("li");
+    so3.classList.add("social");
+    navUl.appendChild(so3);
+    const so3A = document.createElement("a");
+    so3.appendChild(so3A);
+    so3A.href = "#";
+    const icon3 = document.createElement("i");
+    icon3.classList.add("fa", "fa-instagram");
+    so3A.appendChild(icon3);
+
+    return nav
+}
+
+const createHead = () => {
+    const head = document.createElement("div");
+    head.classList.add("head-section");
+
+    const h1 = document.createElement("h1");
+    h1.innerText = "Yamato Ramen";
+    head.appendChild(h1);
+
+    const h3 = document.createElement("h3");
+    h3.innerText = "Yummy ラーメン";
+    head.appendChild(h3);
+
+    const p1 = document.createElement("p");
+    p1.innerText = "Autentic japanese style Ramen for every taste.";
+
+    const ul = document.createElement("ul");
+    p1.appendChild(ul);
+    head.append(p1);
+
+    const li1 = document.createElement("li");
+    li1.innerText = "Shōyu-Ramen";
+    const li2 = document.createElement("li");
+    li2.innerText = "Shio-Ramen";
+    const li3 = document.createElement("li");
+    li3.innerText = "Miso-Ramen";
+
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+
+    const p2 = document.createElement("p");
+    p2.innerText = "Try them all and discover your favorite! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum obcaecati aliquam nisi unde provident ad debitis harum ea sed natus, ut quibusdam inventore, assumenda vel! Molestias maiores temporibus odit provident ratione quos odio impedit laborum magnam sunt, asperiores distinctio et necessitatibus dicta ut doloribus commodi ipsa quidem libero quae. Modi.";
+    p1.appendChild(p2);
+
+    return head
+}
 
 
-
-export { createBackground }
+export {
+    createBackground,
+    createWrapperUpper,
+    createNavWrap,
+    createNav,
+    createHead
+}
