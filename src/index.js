0a40b77cb4e670094367a8aae09bcb7d64dcc642
eryner-z <stfn.bgr@gmail.com>
2021-12-28 16:47:00 +0100

    dynamically create navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-
-import { createBackground } from "./modules/home";
+import {
+    createBackground,
+    createWrapperUpper,
+    createNavWrap,
+    createNav
+} from "./modules/home";
 
 const initialize = (() => {
     const content = document.getElementById("content");
     content.appendChild(createBackground());
+    const wrapperU = content.appendChild(createWrapperUpper());
+    const navWrap = wrapperU.appendChild(createNavWrap());
+    navWrap.appendChild(createNav());
+
 })();
-
-
-
