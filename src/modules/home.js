bb8010d57e7b7867e538c22927e2cdeaed35d5bd
eryner-z <stfn.bgr@gmail.com>
2021-12-28 15:51:58 +0100

    export background image

---
+++ src/modules/home.js
@@ -0,0 +1,15 @@
+
+
+const createBackground = () => {
+    const image = document.createElement("img");
+    image.classList.add("background-image")
+    image.src = "../src/images/ramen-background.jpg";
+    image.alt = "tasty ramen";
+    console.log(image);
+    return image
+}
+
+
+
+
+export { createBackground }
